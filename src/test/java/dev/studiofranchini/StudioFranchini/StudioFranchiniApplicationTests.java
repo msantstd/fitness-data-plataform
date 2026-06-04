f536dbb93606f7a85bf14273e956b0aaa1efc003
msantstd <marcios.020@gmail.com>
2026-06-04 20:37:17 -0300

    Iniciando repositório Git

---
+++ src/test/java/dev/studiofranchini/StudioFranchini/StudioFranchiniApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.studiofranchini.StudioFranchini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioFranchiniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
